30980445de1bfc1fe4150775061ccfc4f8e18601
keprom <kokshetau.energo.prom@mail.ru>
2018-12-28 09:27:18 +0600

    added kontragent rek

--- system/application/views/reports.php
+++ system/application/views/reports.php
@@ -55,3 +55,4 @@ echo anchor("billing/pre_manual_sf","Счет-фактура")."<br>";
 <a href="<?php echo site_url('billing/pre_tenge_year'); ?>"><?php echo 'Свод начислений по годам в разрезе фирм'; ?></a><br>
 <a href="<?php echo site_url('billing/double_oplata'); ?>"><?php echo 'Дублирующиеся оплаты'; ?></a><br>
 <a href="<?php echo site_url('billing/pre_billing_info_dop'); ?>"><?php echo 'Информация по точкам учета по ТУРЭ'; ?></a><br>
+<li><a href="<?php echo site_url('billing/kontragent_rek'); ?>"><?php echo 'Список контрагентов с реквизитами'; ?></a></li>
