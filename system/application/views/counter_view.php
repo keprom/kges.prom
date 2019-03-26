7ada2868ef29e39bb16aca579aaeaa725017d3cd
keprom <kokshetau.energo.prom@mail.ru>
2019-03-26 17:32:03 +0600

    added back button

--- system/application/views/counter_view.php
+++ system/application/views/counter_view.php
@@ -1,3 +1,5 @@
+<?php echo anchor(base_url()."/billing/point/{$point_id}","Назад к списку счетчиков" )."";?><br><br>
+
 <b>Тарифы счетчика </b> <br/><br/><br/>
 <b>установленные тарифы на счетчике:</b><br/><br/>
 <?php echo anchor ("billing/change_counter/".$counter_id,"Редактировать счетчик" );?><br><br><br>
